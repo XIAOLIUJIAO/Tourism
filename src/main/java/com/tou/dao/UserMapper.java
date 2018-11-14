dcf52c0390b9f90198a8dcf696bea088e3b5ac6c
XIAOLIUJIAO <2911627187@qq.com>
2018-11-14 13:28:36 +0800

    测试及生成文件

--- src/main/java/com/tou/dao/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.tou.dao;
-
-import com.tou.entity.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserMapper {
-    public User login(@Param("username") String username, @Param("password") String password);
-}
--- src/main/java/com/tou/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.tou.service;
-
-import com.tou.entity.User;
-
-public interface UserService {
-    public User login(String username,String password);
-}
